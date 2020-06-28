a92b699eabbb9175fedee65182e97a80adf04baf
whuandra <winoto@hotmail.com>
2020-06-28 15:30:46 -0700

    correction line 10

--- aws_vpc_tfc.tf
+++ aws_vpc_tfc.tf
@@ -7,11 +7,11 @@ provider "aws" {
 
 # VPC resources: This will create 1 VPC with 4 subnets, 1 IGW, 4 RT
 
-resource "aws_vpc" "default" (
+resource "aws_vpc" "default" {
     cidr_block = var.cidr_block
     enable_dns_support = true
     enable_dns_hostname = true
-)
+}
 
 resource "aws_internet_gateway" "default"{
     vpc_id = aws_vpc.default.id
